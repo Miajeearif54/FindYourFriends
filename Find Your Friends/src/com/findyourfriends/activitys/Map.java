b9fc82a0ad18a58da3ed6c7c1ef3a91e0356e209
wertonguimaraes <werton.gomes@embedded.ufcg.edu.br>
2014-08-13 19:20:45 -0300

    alterando o jeito de  chamar a AsyncTask

--- Find Your Friends/src/com/findyourfriends/activitys/Map.java
+++ Find Your Friends/src/com/findyourfriends/activitys/Map.java
@@ -67,7 +67,9 @@ public class Map extends Activity implements LocationListener {
         grupos.setOnClickListener(new View.OnClickListener() {	
 			@Override
 			public void onClick(View v) {
-			    new CapturaJSON().execute();
+			    Intent i = new Intent(mContext, ViewGroupActivity.class);
+                startActivity(i);
+			    
 			}
 		});
               
@@ -160,45 +162,7 @@ public class Map extends Activity implements LocationListener {
         locationManager.removeUpdates(this);
     }
     
-    private class CapturaJSON extends AsyncTask<Void, Void, List<Grupo>> {
-        private ProgressDialog dialog;
-
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-            
-            //dialog = ProgressDialog.show(Map.this, "Espere", "Sincronizando os dados");
-        }
-
-        @Override
-        protected List<Grupo> doInBackground(Void... params) {
-            return getJSON();
-        }
-
-        @Override
-        protected void onPostExecute(List<Grupo> result) {
-            super.onPostExecute(result);
-            
-//            Session.delInstancia();
-//            Session.getInstancia().setDono(email); //login.getText().toString()
-//            
-//            for (Ti ti : result) {
-//                Session.getInstancia().getAtividades().add(ti);
-//            }
-            
-            Intent i = new Intent(mContext, ViewGroupActivity.class);
-            startActivity(i);
-            
-            Log.d("werton", result.size()+"");
-            
-            //dialog.dismiss();
-        }
-
-        private List<Grupo> getJSON() {
-            JSONParse parser = new JSONParse("aqui deve ter um link");
-            return parser.getGruposBD();
-        }
-    }
+    
 
 	
 }
--- Find Your Friends/src/com/findyourfriends/activitys/ViewGroupActivity.java
+++ Find Your Friends/src/com/findyourfriends/activitys/ViewGroupActivity.java
@@ -42,9 +42,50 @@ public class ViewGroupActivity extends Activity{
 			}
 		});
         
+        new CapturaJSON().execute();
+        
         
 		
 	}
+	
+	private class CapturaJSON extends AsyncTask<Void, Void, List<Grupo>> {
+        private ProgressDialog dialog;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            
+            //dialog = ProgressDialog.show(Map.this, "Espere", "Sincronizando os dados");
+        }
+
+        @Override
+        protected List<Grupo> doInBackground(Void... params) {
+            return getJSON();
+        }
+
+        @Override
+        protected void onPostExecute(List<Grupo> result) {
+            super.onPostExecute(result);
+            
+//            Session.delInstancia();
+//            Session.getInstancia().setDono(email); //login.getText().toString()
+//            
+//            for (Ti ti : result) {
+//                Session.getInstancia().getAtividades().add(ti);
+//            }
+            
+            
+            
+            Log.d("werton", result.size()+"");
+            
+            //dialog.dismiss();
+        }
+
+        private List<Grupo> getJSON() {
+            JSONParse parser = new JSONParse("aqui deve ter um link");
+            return parser.getGruposBD();
+        }
+    }
 	
 	
 	
