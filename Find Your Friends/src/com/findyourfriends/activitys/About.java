8294f33137f7a57d8bfe5497a593d2aee4d91433
renanpintos <renan.pinto.s@gmail.com>
2014-09-05 18:45:16 -0300

    Tela About e mudanca de interface (icones superiores)

---
+++ Find Your Friends/src/com/findyourfriends/activitys/About.java
@@ -0,0 +1,24 @@
+package com.findyourfriends.activitys;
+
+import com.les.findyourfriends.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * This class extends Activity and shows a screen with a brief application's
+ * description and the developers's names.
+ */
+
+public final class About extends Activity {
+
+    /*
+     * (non-Javadoc)
+     * @see android.app.Activity#onCreate(android.os.Bundle)
+     */
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.about);
+    }
+}
