175ee0cb6b55deb4e99106bbb80041842fea57db
digust10 <ernest.diego@gmail.com>
2014-11-03 10:16:06 -0300

    Verificacao de conexao

--- Find Your Friends/src/com/findyourfriends/activitys/MainActivity.java
+++ Find Your Friends/src/com/findyourfriends/activitys/MainActivity.java
@@ -3,18 +3,22 @@ package com.findyourfriends.activitys;
 import java.io.InputStream;
 
 import android.app.Activity;
+import android.app.Dialog;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentSender.SendIntentException;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -75,6 +79,9 @@ ConnectionCallbacks, OnConnectionFailedListener{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
+		//testar conexao
+		haveNetworkConnection();
+		
 		//--google
 				continuar = (Button) findViewById(R.id.buttonContinuar);
 				btnSignIn = (SignInButton) findViewById(R.id.btn_sign_in);
@@ -105,20 +112,45 @@ ConnectionCallbacks, OnConnectionFailedListener{
 		continuar.setOnClickListener(new View.OnClickListener() {	
 			@Override
 			public void onClick(View v) {
+		         if (haveNetworkConnection()){
+		                Log.d("internet", "tem internet");
+		                new ListUsers().execute(email);
+		                
+		                
+		                Session.delInstancia();
+		                Session.getInstancia().setDono(email); //login.getText().toString()
+		                
+		                Intent i = new Intent(mContext, Map.class);
+		                i.putExtra("mostrar_botoes", true);
+		                startActivity(i);
+		          } else {
+		              confirmacaoDeRede();
+		          }
 			    
-			    new ListUsers().execute(email);
-			    
-			    
-			    Session.delInstancia();
-	            Session.getInstancia().setDono(email); //login.getText().toString()
-	            
-				Intent i = new Intent(mContext, Map.class);
-                i.putExtra("mostrar_botoes", true);
-				startActivity(i);
 			}
 		});
 		
 	}
+
+	private void confirmacaoDeRede() {
+        final Dialog alertDialog = new Dialog(this);
+        alertDialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        alertDialog.setContentView(R.layout.confirmacao_internet_dialog);
+        TextView tvMsgToShow = (TextView) alertDialog.findViewById(R.id.conexao_texto);        
+        
+        Button ok = (Button) alertDialog.findViewById(R.id.sem_conexao_ok);
+        ok.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                alertDialog.dismiss();
+            }
+        });
+        
+        alertDialog.show();
+
+    }
+
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -242,8 +274,29 @@ ConnectionCallbacks, OnConnectionFailedListener{
 	        }
 	    }
 	    
-	    //login----
-	    private void signInWithGplus() {
+	    
+	    
+	    private boolean haveNetworkConnection() 
+	    {
+	        boolean haveConnectedWifi = false;
+	        boolean haveConnectedMobile = false;
+
+	        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+	        NetworkInfo[] netInfo = cm.getAllNetworkInfo();
+	        for (NetworkInfo ni : netInfo) 
+	        {
+	            if (ni.getTypeName().equalsIgnoreCase("WIFI"))
+	                if (ni.isConnected())
+	                    haveConnectedWifi = true;
+	            if (ni.getTypeName().equalsIgnoreCase("MOBILE"))
+	                if (ni.isConnected())
+	                    haveConnectedMobile = true;
+	        }
+	        return haveConnectedWifi || haveConnectedMobile;
+	    }
+	    
+	    //login----------------------------
+	    private void signInWithGplus() {	        
 	        if (!mGoogleApiClient.isConnecting()) {
 	            mSignInClicked = true;
 	            resolveSignInError();
